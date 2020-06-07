e7f6895cee44ffe2b65c8c6d43c54deaabe40db3
IMCorn <clay456789@gmail.com>
2020-06-08 02:32:35 +0800

    建立多型跟抽象的專案

---
+++ DoSomething/PolymorphismClass/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parent obj = new Child();
+            obj.Name = "Corn";
+            obj.print();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Parent
+    {
+        public string Name { get; set; }
+        public void print()
+        {
+            Console.WriteLine($"Parent Name : {Name}");
+        }
+
+        public virtual void Print()
+        {
+
+        }
+    }
+
+    abstract class A
+    {
+        public abstract void BBB();
+    }
+
+    class Child : Parent
+    {
+        new public void print()
+        {
+            Console.WriteLine($"Child Name : {Name}");
+        }
+    }
+}
