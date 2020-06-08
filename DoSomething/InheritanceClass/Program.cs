f5c5f77393dc7096a4404821c5e8def77baa4688
Corn Yang <clay456789@gmail.com>
2020-06-08 14:29:26 +0800

    修正衝突&繼承&封裝
    1. 衝突修正
    2. 增加繼承的註解
    3. 封裝專案ToDo

--- DoSomething/InheritanceClass/Program.cs
+++ DoSomething/InheritanceClass/Program.cs
@@ -14,10 +14,11 @@ namespace InheritanceClass
     {
         static void Main(string[] args)
         {
+            //new  一個有繼承的類別 object，可以使用他繼承的裡面的東西
             Program p = new Program();
             p.Help();
 
-
+            //如果沒有繼承，則必須使用new 一個object 來使用其裡面的方法
             NoHelper noHelper = new NoHelper();
             noHelper.NoHelp();
 
---
+++ DoSomething/PackageClass/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageClass
+{
+    /// <summary>
+    /// 封裝練習
+    /// </summary>
+    class Program : Helper
+    {
+        static void Main(string[] args)
+        {
+            //如果使用private，不管事繼承或者是new 物件來使用其private會無法使用
+            Program p = new Program();
+            p.Title2();
+            Helper helper = new Helper();
+            helper.Title2();
+
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Helper
+    {
+        private void Title()
+        {
+            Console.WriteLine("Private HelloWorld");
+        }
+
+        public void Title2()
+        {
+            Console.WriteLine("Public HelloWorld");
+        }
+
+        protected void Title3()
+        {
+            Console.WriteLine("Protected HelloWorld");
+        }
+    }
+}
