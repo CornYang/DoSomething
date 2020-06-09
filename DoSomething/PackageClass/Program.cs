889668f5294e5b9537da87ade6e19c784a2fb746
Corn Yang <clay456789@gmail.com>
2020-06-09 18:01:34 +0800

    多型 & 多載
    1. 完成多型
    2. 完成多載

--- DoSomething/PackageClass/Program.cs
+++ DoSomething/PackageClass/Program.cs
@@ -13,12 +13,22 @@ namespace PackageClass
     {
         static void Main(string[] args)
         {
-            //如果使用private，不管事繼承或者是new 物件來使用其private會無法使用
+            //如果使用private的封裝，不管是繼承或者是new 物件來使用其private方法會無法使用
+            //如果使用protected的封裝，只有自己及繼承這Class的人才能使用
+            //如果使用internal的封裝，只能在相同專案中使用
+            //如果使用protected internal的封裝，只有相同專案且只有自己及繼承的人可以使用
+
             Program p = new Program();
             p.Title2();
+            p.Title3();
+            p.Title4();
+            p.Title5();
+            Console.WriteLine("-------------------------------------------------------");
+
             Helper helper = new Helper();
             helper.Title2();
-
+            helper.Title4();
+            helper.Title5();
 
             Console.ReadKey();
         }
@@ -39,6 +49,16 @@ namespace PackageClass
         protected void Title3()
         {
             Console.WriteLine("Protected HelloWorld");
+        }
+
+        internal void Title4()
+        {
+            Console.WriteLine("Internal HelloWorld");
+        }
+
+        protected internal void Title5()
+        {
+            Console.WriteLine("Protected Internal HelloWorld");
         }
     }
 }
