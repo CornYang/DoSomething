6e0270a45743cdee056df3f194ce9e34a3b31fe3
Corn Yang <clay456789@gmail.com>
2020-06-11 17:56:34 +0800

    Enum & Overload
    1. 進行Enum專案
    2. 修改Overload而不是Override

---
+++ DoSomething/EnumSample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumSample
+{
+    /// <summary>
+    /// Enum列舉練習
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gender gender = Gender.Boy;
+            Console.WriteLine(gender.ToString());
+
+            Console.WriteLine(Days.Sun);
+
+            Console.ReadKey();
+        }
+    }
+
+    public enum Gender { Boy, Girl}
+    public enum Days : byte //自訂義
+    { Sat = 1, Sun, Mon, Tue, Wed, Thu, Fri }
+}
---
+++ DoSomething/OverloadSample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadSample
+{
+    /// <summary>
+    /// 多載(Override)練習
+    /// </summary>
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.R("HelloWorld");
+            Console.WriteLine(p.R(5));
+            Console.WriteLine(p.R(2.0));
+
+
+            Console.ReadKey();
+        }
+
+        public void R(string a)
+        {
+            Console.WriteLine(a);
+        }
+
+        public int R(int a)
+        {
+            return a + 5;
+        }
+
+        public bool R(double a)
+        {
+            return a > 5;
+        }
+    }
+}
