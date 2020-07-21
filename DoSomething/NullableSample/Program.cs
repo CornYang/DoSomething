d0f5497b872675e0090aabe54a1da6ee7898fce5
Corn Yang <clay456789@gmail.com>
2020-07-21 15:00:40 +0800

    StructTricky&Nullable Practice
    1. StructTricky 各種情境練習
    2. 練習Nullable狀態
    3. 練習比較Nullable

---
+++ DoSomething/NullableSample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullableSample
+{
+    /// <summary>
+    /// Nullable<T>練習
+    /// Nullable Compare
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<int> i; // == int? i ==  System.Int32? i
+            i = 100;
+            Console.WriteLine(i.HasValue.ToString());
+            Console.WriteLine(i.Value.ToString());
+            Console.WriteLine(i.GetValueOrDefault().ToString());
+            i = null;
+            Console.WriteLine(i.HasValue.ToString());
+            //Console.WriteLine(i.Value.ToString()); //因為為Null，所以i.Value並無Value
+            Console.WriteLine(i.GetValueOrDefault().ToString());
+            Console.WriteLine("----------------------------------------------------------");
+
+            int? x = null;
+            int y = 0;
+            Console.WriteLine(x == y);
+            Console.WriteLine(x != null);
+            Console.WriteLine(x.HasValue);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DoSomething/StructureTrickySample/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructureTrickySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStruct myStruct = new MyStruct() { X = 1, Y = 1 };
+            var myStruct2 = myStruct;
+            myStruct2.X = 2;
+
+            Console.WriteLine($"myStruct.X = {myStruct.X}");
+            Console.WriteLine($"myStruct2.X = {myStruct2.X}");
+            Console.WriteLine("----------------------------------------------------------");
+
+            List<MyStruct> list = new List<MyStruct>();
+            MyStruct a = new MyStruct() { X = 1, Y = 1 };
+            list.Add(a);
+            a.X = 999;
+            Console.WriteLine(list[0].X);
+            var b = list[0];
+            b.X = 666;
+            list[0] = b;
+            Console.WriteLine(list[0].X);
+            Console.WriteLine(a.X);
+            Console.WriteLine("----------------------------------------------------------");
+
+            MyStruct[] arr = new MyStruct[2];
+            arr[0].X = 111;
+            var o = arr[0];
+            Console.WriteLine(o.X);
+            o.X = 222;
+            Console.WriteLine(arr[0].X);
+            arr[0].X = 333;
+            Console.WriteLine(arr[0].X);
+
+            Console.ReadKey();
+        }
+    }
+
+    public struct MyStruct
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
