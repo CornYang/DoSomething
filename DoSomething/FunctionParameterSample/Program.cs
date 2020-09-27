f9b604ad7fa5609b589617bcfea0b2a08723f10c
Corn Yang <clay456789@gmail.com>
2020-09-27 16:14:41 +0800

    Function & Parameter
    1. 練習方法參數傳遞
    2. 了解callbyvaule & callbyref

--- DoSomething/FunctionParameterSample/Program.cs
+++ DoSomething/FunctionParameterSample/Program.cs
@@ -10,6 +10,111 @@ namespace FunctionParameterSample
     {
         static void Main(string[] args)
         {
+            int val = 0;
+            Console.WriteLine($"val的初始值為 {val}");
+            int res = Function01(val);
+            Console.WriteLine($"退出Function01方法回到Main方法後，val的值為 {val}");
+            Console.WriteLine("-----------------------------------------------------------------------");
+
+            int val2 = 0;
+            Console.WriteLine($"val的初始值為 {val2}");
+            int res02 = Function02(ref val2);
+            Console.WriteLine($"退出Function02方法回到Main方法後，val2的值為 {val2}");
+            Console.WriteLine("-----------------------------------------------------------------------");
+
+            ParaClass para = new ParaClass(); //先實體化ParaClass，配給para
+            Console.WriteLine($"para實體中的val初始值為 {para.val}");
+            ParaClass r = Function03(para);
+            Console.WriteLine($"退出Function03方法回到Main方法後，para實體中的val值為 {para.val}");
+            Console.WriteLine("-----------------------------------------------------------------------");
+
+            ParaClass para2 = new ParaClass();
+            Console.WriteLine($"para2實體中的val初始值為 {para2.val}");
+            ParaClass res03 = Function04(ref para2);
+            Console.WriteLine($"退出Function04方法回到Main方法後，para2實體中的val值為 {para2.val}");
+            Console.WriteLine("Function04 回傳的實體中的 val 的值為 " + para2.val.ToString());
+            Console.WriteLine("-----------------------------------------------------------------------");
+
+            ParaClass p = new ParaClass();
+            ParaClass r1 = ChangeByValue(p);
+            Console.WriteLine(string.Format("r1 和 p 指向同實體 : {0}", (r1 == p)));
+            ParaClass r2 = ChangeByRef(ref p);
+            Console.WriteLine(string.Format("r2 和 p 指向同實體 : {0}", (r2 == p)));
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// 取出 Main 方法中 val 的值複製一份到 Function01 方法中的 val(兩個 val 的變數位址不同)
+        /// </summary>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        private static int Function01(int val)
+        {
+            Console.WriteLine($"進入方法Function01時，val的初始值為 {val}");
+            val = 99;
+            Console.WriteLine($"在Function01方法裡重新指派值後，val的值為 {val}");
+            return val;
+        }
+
+        /// <summary>
+        /// 取出 Main 方法中 val2 的位址傳遞給 Function02 方法中的 val2 (兩個 val2 的變數位址相同)
+        /// </summary>
+        /// <param name="val2"></param>
+        /// <returns></returns>
+        private static int Function02(ref int val2)
+        {
+            Console.WriteLine($"進入方法Function01時，val2的初始值為 {val2}");
+            val2 = 999;
+            Console.WriteLine($"在Function01方法裡重新指派值後，val2的值為 {val2}");
+            return val2;
+        }
+
+        public class ParaClass
+        {
+            public int val = 0;
+        }
+
+        /// <summary>
+        /// 取出 Main 方法中 para 的值複製一份到 Function03 方法中的 para(兩個 para 的變數位址不同)
+        /// </summary>
+        /// <param name="para"></param>
+        /// <returns></returns>
+        private static ParaClass Function03(ParaClass para)
+        {
+            Console.WriteLine($"進入方法Function03時候，para實體中val值為 {para.val}");
+            para.val = 50;
+            Console.WriteLine($"在Function03方法裡重新指派值給para的val，para實體中的val值為 {para.val}");
+            para = new ParaClass();
+            Console.WriteLine($"在Function03方法裡重新產生ParaClass的實體para，para實體中的val值為 {para.val}");
+            return para;
+        }
+
+        private static ParaClass Function04(ref ParaClass para2)
+        {
+            Console.WriteLine($"進入方法Function04時候，para2實體中val值為 {para2.val}");
+            para2.val = 500;
+            Console.WriteLine($"在Function04方法裡重新指派值給para2的val，para2實體中的val值為 {para2.val}");
+            para2 = new ParaClass();
+            Console.WriteLine($"在Function04方法裡重新產生ParaClass的實體para，para實體中的val值為 {para2.val}");
+            return para2;
+        }
+
+        //pass by value傳遞，array變數是另開一個空間(0x20)，來存這個陣列的所在位置(0x10)，但透過pass by referencs，ref array變數的位置就是跟原本arr變數一樣(0x00)，所以value也一樣(0x10)。 
+        //差別就是，如果你在函式內想new一個陣列給array(等於給array變數新的參考)，就會發現pass by value完全沒有改變，
+        //因為array跟原本的arr完全沒有關係，而pass by reference就成功new了
+        //http://davidhsu666.com/archives/c-sharp-call-by-value-and-call-by-reference-and-equals/
+
+
+        private static ParaClass ChangeByValue(ParaClass p)
+        {
+            p = new ParaClass();
+            return p;
+        }
+
+        private static ParaClass ChangeByRef(ref ParaClass p)
+        {
+            p = new ParaClass();
+            return p;
         }
     }
 }
